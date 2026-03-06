b4ca1c4c074d312f9330a767327d36fbebd804bb
Aldiyar <aldiyar.zhartybek@narxoz.kz>
2026-03-06 21:25:58 +0500

    add battle core (Combatant, EncounterResult, BattleEngine)

--- src/com/narxoz/rpg/battle/Combatant.java
+++ src/com/narxoz/rpg/battle/Combatant.java
@@ -1,33 +1,8 @@
-package com.narxoz.rpg.hw3.battle;
-
-import java.util.Random;
+package com.narxoz.rpg.battle;
 
 public interface Combatant {
-
-    final class Attack {
-        private final String name;
-        private final int damage;
-
-        public Attack(String name, int damage) {
-            this.name = name;
-            this.damage = damage;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getDamage() {
-            return damage;
-        }
-    }
-
     String getName();
-    int getHealth();
-    int getDefense();
-    int getSpeed();
+    int getAttackPower();
+    void takeDamage(int amount);
     boolean isAlive();
-
-    Attack attack(Random random);
-    void takeDamage(int damage);
 }
